1cc84fc2be0c3fee3b3a21379f85c3e08e3e9542
username <bogang2000@nate.com>
2019-02-15 13:37:40 +0900

    initial commit
    Signed-off-by: username <bogang2000@nate.com>

---
+++ mul.py
@@ -0,0 +1,2 @@
+A,B = map(int,input().split())
+if A<10 and A>0 and B<10 and B>0: print(A*B)
---
+++ remainder.py
@@ -0,0 +1,8 @@
+A,B,C = map(int,input().split())
+
+if A>=2 and A<=10000 and B>=2 and B<=10000 and C>=2 and C<=10000:
+    print((A+B)%C)
+    print((A%C + B%C)%C)
+    print((A*B)%C)
+    print((A%C * B%C)%C)
+      
